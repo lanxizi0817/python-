7524396abb2a63deb118400df0233888ce722a19
lanxizi <3457371265@qq.com>
2025-02-04 17:07:35 +0800

    B2054

---
+++ B2054.py
@@ -0,0 +1,23 @@
+n = int(input())
+num = 0
+while 1<= n <=100:
+    L =[]
+    L = int(L)
+    for j in range(2):
+        L.append(int,input())
+        while 15<= L <= 25:
+            for i in L:
+                num = num + i
+                a = float(num / n)
+                print("%.2f"%a)
+            break
+
+
+sum = float(0)
+x = 0
+n = int(input())
+for i in range(n):
+    x = float(input())
+    sum = sum + x
+a = float(sum/n)
+print("%.2f"%a)
