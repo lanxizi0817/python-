316ad0fa13d2a9d1e6d749dae5e01672a5c7a0d6
lanxizi <3457371265@qq.com>
2025-02-05 23:14:44 +0800

    .

---
+++ 10.30.py
@@ -0,0 +1,9 @@
+if __name__=='__main__':
+    name = input()
+    age = int(input())
+    print("我的名字是 %s，我今年 %d 岁。"%(name,age))
+if __name__=='__main__':
+    student_id=int(input())
+    grade=float(input())
+    course=input()
+    print("学生编号为%d，在%s课程中的成绩是%f。"%(student_id,course,grade))
